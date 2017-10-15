0ef0d693a00f67dbe5878d9dc0f128dea5e78280
Luciany Laurentino <lucylaurentino@gmail.com>
2017-10-15 17:58:40 +1300

    finish loop

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let password = document.querySelector('#password')
 let confirm_password = document.querySelector('#confirm_password')
 let message1 = document.querySelector('#message1')
 let message2 = document.querySelector('#message2')
-let message3 = document.querySelector('#message3')
 let submit = document.querySelector('#submit')
 
 //Creating a function for username
@@ -15,25 +14,10 @@ let ValidateUsername = () => {
 }
 //Creating a function for password
 let ValidatePassword = () =>{
-if(password.value.length > 4) {
-    return true;
-}
-else {
-        message2.innerHTML = "Password must be at least four digits";
-        return false;
-    }
-
-     if (password.value === confirm_password.value ) {
+if (password.value === confirm_password.value ) {
          return true;
-
     } else {
-        message3.innerHTML = "Password doesn't match. Please try again";
+        message2.innerHTML = "Passwords dont match. Please try again";
         return false;
     }
-    
 }
-
-
-
-//Event Listener 
-submit.addEventListener('click', (e) => { submitForm(); e.preventDefault() })
